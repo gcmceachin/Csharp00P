c50202d8d4d675eaa9f9f1def07994f9ed8d414d
gcmceachin <gcmceachin@gmail.com>
2020-04-22 21:02:57 -0400

    FIrst Commit

---
+++ Csharp00P/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp00P
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
